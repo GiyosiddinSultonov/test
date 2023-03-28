09c09990b540ff4be87a95ce015e52385a6070c2
GiyosiddinSultonov <giyosiddinsultonov941@gmail.com>
2023-03-28 08:18:26 +0500

    welcome to dubai

--- src/najottalim.java
+++ src/najottalim.java
@@ -1,6 +1,6 @@
 public class najottalim {
     public static void main(String[] args) {
         System.out.println("Assalomu alaykum");
-        System.out.println("ozgarish 1");
+        System.out.println("welcome to github");
     }
 }
