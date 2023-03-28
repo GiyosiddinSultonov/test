f0088caa77b3eeb2e9da1e9966021d6d89d0b428
GiyosiddinSultonov <giyosiddinsultonov941@gmail.com>
2023-03-28 15:25:57 +0500

    test

--- src/najottalim.java
+++ src/najottalim.java
@@ -1,7 +1,6 @@
 public class najottalim {
     public static void main(String[] args) {
         System.out.println("Assalomu alaykum");
-        System.out.println("welcome to github");
         System.out.println("G'iyosiddin");
         System.out.println("Sultonov");
         System.out.println("G'afurjonuvich");
